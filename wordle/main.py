ad9c1dd28c27340216fa078741e9bbc671d6761a
ah7255703 <ah7255703@gmail.com>
2022-07-17 22:41:02 +0200

    fixes and code ++

--- wordle/main.py
+++ wordle/main.py
@@ -1,51 +1,44 @@
+from functools import wraps
 from typing import Counter, List
-
 from typing_extensions import Self
-
-from .utils import read,save
+from .utils import read, save
 
 
 class Word:
-    def __init__(self,word:str):
-        _word =word.strip().lower()
-        if len(_word) != 5 :
-            raise Exception('Word {} must be 5 chars'.format(_word))
-
-        else:
-            self.__word = _word
-
+    def __init__(self, word: str):
+        self.__word = word.strip().lower()
 
     def __str__(self) -> str:
-        return 'word=' + self.__word
+        return f"{self.__word=}"
 
     @property
     def counter(self):
         return Counter(self.__word)
 
-    def contains(self,letter:str):
+    def contains(self, letter: str):
         return self.__word.__contains__(letter)
-    
+
     @property
     def letters(self):
         return list(self.__word)
-    
+
     @property
     def word(self):
         return self.__word
-    
-    def get_index(self,index:int)->str:
+
+    def get_index(self, index: int) -> str:
         "get letter by it's index"
         if index > 5 or index < 1:
-            raise Exception('index must be from 1 to 5')
+            raise Exception("index must be from 1 to 5")
         else:
-            return self.__word[index-1]
-    
+            return self.__word[index - 1]
+
 
 class WordSet:
-    def __init__(self,file_name:str='wordslist') -> None:
-        self.__words:List[Word]
-        self.file_name = 'wordslist'
-    
+    def __init__(self, file_name: str = "wordslist") -> None:
+        self.__words: List[Word]
+        self.file_name = file_name
+
     def __load(self):
         return read(self.file_name)
 
@@ -53,51 +46,50 @@ class WordSet:
         words = self.__load()
         self.__words = [Word(word) for word in words]
         return self
-    
-    def not_contain(self,letter:str):
-        words = []
-        for word in self.__words:
-            if not word.contains(letter.lower()):
-                words.append(word)
-        self.__words = list(set(words))
+
+    def not_contain(self, letter: str):
+        self.__words = set(
+            list(filter(lambda word: not word.contains(letter.lower()), self.__words))
+        )
         return self
 
-    def letter_at(self,letter:str,index:int):
-        words = []
-        for word in self.__words:
-            if word.get_index(index) == letter.lower():
-                words.append(word)
-        self.__words = words
+    def letter_at(self, letter: str, index: int):
+        self.__words = set(
+            list(
+                filter(
+                    lambda word: word.get_index(index) == letter.lower(), self.__words
+                )
+            )
+        )
         return self
 
-    def contains(self,letter:str):
-        words = []
-        for word in self.__words:
-            if word.contains(letter.lower()):
-                words.append(word)
-        self.__words  = words
+    def unique(self) -> Self:
+        self.__words = list(set(filter(lambda word:len(word.word) == len(set(word.word)) ,self.__words)))
         return self
 
-    def save(self,file_name):
+    def contains(self, letter: str):
+        self.__words = filter(lambda word: word.contains(letter.lower()), self.__words)
+        return self
+
+    def save(self, file_name):
         if self.__words != None:
-            save([word.word for word in self.__words],file_name)
-    
+            save([word.word for word in self.__words], file_name)
+
     @property
     def get_most_repeated_chars(self):
         return sorted(self.counter_all)
 
-
-    @property    
-    def words(self) ->List[Word]:
+    @property
+    def words(self) -> List[Word]:
         return self.__words
 
     @property
-    def count(self) ->int:
+    def count(self) -> int:
         return len(self.__words)
-    
+
     @property
-    def counter_all(self)->List[Counter]:
+    def counter_all(self) -> List[Counter]:
         return [i.counter for i in self.__words]
-    
+
     def __str__(self) -> str:
-        return f'words = {self.count} from {self.file_name}'
+        return f"words = {self.count} from {self.file_name}"
--- wordle/utils.py
+++ wordle/utils.py
@@ -1,31 +1,24 @@
 from typing import Iterable, List, Set
-import tqdm
 
-def read(file_name:str)->List[str]:
-    with open(f'{file_name}.txt','r') as file:
+def read(file_name: str) -> List[str]:
+    with open(f"{file_name}.txt", "r") as file:
         lines = file.readlines()
     return [line.strip().lower() for line in lines]
 
-def get_5_only(lines:List[str])->Set[str]:
-    words = []
-    for word in tqdm.tqdm(lines):
-        if len(word) == 5:
-            words.append(word)
-    return set(words)
+
+def get_5_only(words: List[str]) -> Set[str]:
+    return set(list(filter(lambda line: len(line) == 5, words)))
 
 
-def save(iterable:Iterable[str],file_name:str):
-    with open(f'{file_name}.txt',"w") as file:
+def save(iterable: Iterable[str], file_name: str):
+    with open(f"{file_name}.txt", "w") as file:
         for i in iterable:
-            file.write(i + '\n')
+            file.write(i + "\n")
+
+
+def non_rep_chars(words: List[str]) -> Set:
+    return set(list(filter(lambda word: len(word) == len(set(word)), words)))
 
 
 
-def non_rep_chars(words:List[str]) ->Set:
-    __words = {i if len(i) == len(set(i)) else 'aaaaaa' for i in words}
-    # __words = []
-    # for i in words:
-    #     if len(i) == len(set(i)):
-    #         __words.append(i)
-    return __words
 
